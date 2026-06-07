d50c558644fe29701c8eb10eae492deb981a8572
bhimishettilohith <bhimishettilohith24@gmail.com>
2026-06-07 11:36:06 +0530

    Day 6: Advanced analytics and risk metrics completed

--- scripts/recommender.py
+++ scripts/recommender.py
@@ -1,1 +1,20 @@
-#to be implemented
+import pandas as pd
+
+performance = pd.read_csv(
+    "../data/processed/clean_performance.csv"
+)
+
+def recommend_funds(risk_level):
+
+    filtered = performance[
+        performance["risk_grade"] == risk_level
+    ]
+
+    return filtered.sort_values(
+        "sharpe_ratio",
+        ascending=False
+    ).head(3)
+
+print(
+    recommend_funds("Moderate")
+)
